a20bda6ea6a710d012519f08a5edc4ababafad71
Not-Your-Code <vineetagarwal.now@gmail.com>
2023-09-23 11:08:34 +0530

    testing

--- server/routes/Query.js
+++ server/routes/Query.js
@@ -5,4 +5,4 @@ const{addQuery,getQueries, regExQuery, catQuery} = require('../controllers/Query
 router.route('/query').get(getQueries).post(addQuery)
 router.route('/regEx').get(regExQuery)
 router.route('/catQuery').get(catQuery)
-module.exports = router
+module.exports = router  
