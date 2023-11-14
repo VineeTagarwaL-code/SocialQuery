9cdfad220f11694c092b18c280cfa0b254911c8e
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2023-11-14 14:06:27 +0530

    fixed a issue while building project

--- client/src/components/layout/navigation/Nav/Navbar.jsx
+++ client/src/components/layout/navigation/Nav/Navbar.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRightFromBracket, faFilter, faMagnifyingGlass, faGear, faBars } from '@fortawesome/free-solid-svg-icons'
 import { ToastContainer, toast } from 'react-toastify';
-import './Navbar.css'
+import './NavBar.css'
 function Navbar({ setQuery }) {
   const navigate = useNavigate();
   function handleLogout() {
